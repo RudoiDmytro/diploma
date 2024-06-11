a38942452d95cea63b0ec74c527cf2b010a806a6
Rudoi Dmytro <irudoj63@gmail.com>
2024-06-11 23:09:57 +0300

    -

--- src/app/[locale]/test-library/[slug]/page.tsx
+++ src/app/[locale]/test-library/[slug]/page.tsx
@@ -7,9 +7,8 @@ import AddTasks from "@/app/components/test/addTasks";
 import { options } from "@/app/components/auth/Options";
 import Link from "next/link";
 import { getServerSession } from "next-auth";
-import { getResults } from "@/lib/serverUtils";
-import TestDetailsPage from "@/app/components/test/TestDetailsPage"
-
+import { getResults, getTasks } from "@/lib/serverUtils";
+import TestDetailsPage from "@/app/components/test/TestDetailsPage";
 
 interface PageProps {
   params: { slug: string };
@@ -38,13 +37,13 @@ export default async function page({ params: { slug } }: PageProps) {
   const assessment = await getAssessment(slug);
   const session = await getServerSession(options);
   const results = await getResults(session?.user.id as string, slug);
-
+  const tasks = await getTasks(slug);
   return (
     <main className="flex flex-col items-center max-md:w-screen max-w-7xl">
       <div className="flex flex-col px-4 max-w-7xl my-10 w-full md:flex-row items-center gap-5 md:items-start">
         <TestDetailsPage task={assessment} />
         <aside className="flex w-fit flex-col gap-5 sticky top-20">
-          {!results && (
+          {!results && tasks.length > 0 && (
             <Button asChild>
               <button className="w-full md:w-fit">
                 <Link href={`/test-library/${slug}/take-assessment`}>
